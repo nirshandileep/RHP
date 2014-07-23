4800f1bb234eec002ffc90ac301c61c99947cfba
nirshandileep <cooldilcool@gmail.com>
2014-07-23 08:21:56 +0530

    Refactor login page.

---
+++ RHP.UserManagement/User.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+namespace RHP.UserManagement
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AccountLocked { get; set; }
+        public string FBAccessToken { get; set; }
+
+        private List<string> rolesList;
+        public List<string> RolesList 
+        {
+            get 
+            {
+                if (rolesList == null)
+                {
+                    rolesList = new List<string>();
+                }
+                
+                rolesList = Roles.GetRolesForUser(UserName).ToList<string>();
+                return rolesList;
+            }
+        }
+             
+
+        public bool AuthenticateUser()
+        {
+            string success = "";
+            bool boolAuthenticated = false;
+            try
+            {
+                // Authenticate Credentials
+                boolAuthenticated = Membership.ValidateUser(UserName, Password);
+                
+                if (boolAuthenticated)
+                    success = "True";
+                else
+                    success = "False";
+
+                if (success == "True")
+                {
+
+                    MembershipUser newUser;
+                    newUser = Membership.GetUser(UserName);
+
+                    UserId = newUser.ProviderUserKey.ToString();
+
+
+                    if (newUser.IsLockedOut)
+                        AccountLocked = "Account Locked Out. Max Password Attempts Reached. Please Contact Admin";
+
+                }
+                /*
+
+                bool pass = Membership.EnablePasswordRetrieval;
+                bool xx = Membership.EnablePasswordReset;
+                string xxx = Membership.GetUser(strUsername).GetPassword();
+                */
+            }
+
+
+            catch (Exception ex)
+            {
+
+            }
+            return boolAuthenticated;
+        }
+
+        public void LogOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        public void RedirectUserFromLogin(bool isUseDefault = true)
+        {
+            if (isUseDefault)
+            {
+                FormsAuthentication.RedirectFromLoginPage(UserName, false);    
+            }
+            else
+            {
+                HttpContext.Current.Response.Redirect(this.RedirectToHomePageByRole());
+            }
+        }
+
+        public string RedirectToHomePageByRole()
+        {
+            string URL = "";
+            string role = "";
+
+            if (RolesList != null || rolesList.Count > 0)
+            {
+                role = RolesList[0].ToString();
+            }
+
+            if (role.ToString() == "Admin")
+            {
+                URL = "~/";
+            }
+            else if (role.ToString() == "LandLoad")
+            {
+                URL = "~/Land_load/Land_load_Profile.aspx";
+            }
+            else if (role.ToString() == "Student")
+            {
+                URL = "~/Student/Student_Profile.aspx";
+            }
+            else if (role.ToString() == "SystemAdmin")
+            {
+                URL = "~/";
+            }
+            else
+            {
+                URL = "~/";
+            }
+
+            return URL;
+
+        }
+
+    }
+}
--- USA_Rent_House_Project/Modules/Login.ascx.cs
+++ USA_Rent_House_Project/Modules/Login.ascx.cs
@@ -5,14 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using RHP.UserManagement;
 
 namespace USA_Rent_House_Project.Modules
 {
     public partial class Login : System.Web.UI.UserControl
     {
-        private static string strAccountLocked = null;
-        private string strAuthUserID;
-        private string strUsername, strPassword;
+
+        private User user = new User();
         
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -21,141 +21,30 @@ namespace USA_Rent_House_Project.Modules
 
         private void login()
         {
-            if (authenticateUser())
+
+            user.UserName = LoginUser.UserName;
+            user.Password = LoginUser.Password;
+
+            if (user.AuthenticateUser())
             {
                 try
                 {
-                    FormsAuthentication.RedirectFromLoginPage(strUsername, false);
-                    Response.Redirect(managerole());
+                    user.RedirectUserFromLogin();
                 }
-
                 catch (Exception ex)
                 {
-                    FormsAuthentication.SignOut();
+                    user.LogOut();
                 }
             }
             else
             {
-                FormsAuthentication.SignOut();
+                user.LogOut();
             }
-        }
-
-        protected string managerole()
-        {
-            string URL = "";
-            string[] userroles = Roles.GetRolesForUser(strUsername);
-         
-            string role = userroles[0].ToString();
-
-            if (role.ToString() == "Admin")
-            {
-                URL = "~/";
-            }
-            else if (role.ToString() == "LandLoad")
-            {
-                URL = "~/Land_load/Land_load_Profile.aspx";
-            }
-            else if (role.ToString() == "Student")
-            {
-                URL = "~/Student/Student_Profile.aspx";
-            }
-            else if (role.ToString() == "SystemAdmin")
-            {
-                URL = "~/";
-            }
-            else
-            {
-                URL = "~/";
-            }
-
-
-            //foreach (var role in userroles)
-            //{
-
-            //    if (role.ToString() == "Admin")
-            //    {
-            //           URL = "~/";
-            //        break;
-            //    }
-            //    else if (role.ToString() == "LandLoad")
-            //    {
-            //          URL = "~/Land_load/Land_load_Profile.aspx";
-            //          break;
-            //    }
-
-            //    else if (role.ToString() == "Student")
-            //    {
-            //        URL = "~/Student/Student_Profile.aspx"; 
-            //        break;
-            //    }
-            //    else if (role.ToString() == "SystemAdmin")
-            //    {
-            //           URL = "~/";
-            //        break;
-            //    }
-            //    else
-            //    {
-            //            URL = "~/";
-            //        break;
-            //    }
-            //}
-
-            return URL;
-                  
-        }
-       
-        protected bool authenticateUser()
-        {
-            string success = "";
-            bool boolAuthenticated = false;
-            try
-            {
-                // Authenticate Credentials
-                boolAuthenticated = Membership.ValidateUser(strUsername, strPassword);
-                if (boolAuthenticated)
-                    success = "True";
-                else
-                    success = "False";
-
-                if (success == "True")
-                {
-
-                    MembershipUser newUser;
-                    newUser = Membership.GetUser(strUsername);
-
-                    strAuthUserID = newUser.ProviderUserKey.ToString();
-
-
-                    if (newUser.IsLockedOut)
-                        strAccountLocked = "Account Locked Out. Max Password Attempts Reached. Please Contact Admin";
-
-                }
-                /*
-
-                bool pass = Membership.EnablePasswordRetrieval;
-                bool xx = Membership.EnablePasswordReset;
-                string xxx = Membership.GetUser(strUsername).GetPassword();
-                */
-            }
-
-
-            catch (Exception ex)
-            {
-
-            }
-            return boolAuthenticated;
         }
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
-            strUsername = LoginUser.UserName;
-
-            strPassword = LoginUser.Password;
             login();
         }
-
-        
-
-
     }
 }
