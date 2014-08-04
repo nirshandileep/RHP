6d72bbbb9a700dc867fd846709564c95a1838c87
look2cool <look2cool@gmail.com>
2014-08-04 16:44:11 +0530

    Student DAO functions

--- RHP.StudentManagement/Student.cs
+++ RHP.StudentManagement/Student.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using RHP.Common;
 using RHP.UserManagement;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
 
 namespace RHP.StudentManagement
 {
@@ -12,5 +14,74 @@ namespace RHP.StudentManagement
         public Guid StudentId { get; set; }
         public Guid StudentUserId { get; set; }
         public Guid SchoolId { get; set; }
+
+        public bool Save()
+        {
+            bool result = false;
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                StudentDAO studentDAO = new StudentDAO();
+                if (studentDAO.IsUserExist(this))
+                {
+                    result = (new UserDAO()).Update(this, db, transaction);
+                }
+                else
+                {
+                    result = studentDAO.Insert(this, db, transaction);
+                }
+
+                transaction.Commit();
+            }
+            catch (System.Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Delete user from the database.
+        /// This only marks the user as IsDeleted
+        /// </summary>
+        /// <returns></returns>
+        public bool Delete()
+        {
+            bool result = false;
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                result = (new StudentDAO().Delete(this, db, transaction));
+
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+
     }
 }
--- RHP.StudentManagement/StudentDAO.cs
+++ RHP.StudentManagement/StudentDAO.cs
@@ -2,10 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+using RHP.Common;
 
 namespace RHP.StudentManagement
 {
     public class StudentDAO
     {
+        public bool Insert(Student student, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_StudentInsert");
+
+            db.AddInParameter(command, "StudentId", DbType.Guid, student.StudentId);
+            db.AddInParameter(command, "UserId", DbType.Guid, student.UserId);
+            db.AddInParameter(command, "SchoolId", DbType.Guid, student.SchoolId);
+            db.AddInParameter(command, "IsDeleted", DbType.String, student.IsDeleted);
+            db.AddInParameter(command, "CreatedBy", DbType.Guid, student.CreatedBy);
+            db.AddInParameter(command, "CreatedDate", DbType.DateTime, student.CreatedDate);
+            db.AddInParameter(command, "UpdatedBy", DbType.Guid, student.UpdatedBy);
+            db.AddInParameter(command, "UpdatedDate", DbType.DateTime, student.UpdatedDate);
+            
+            db.AddOutParameter(command, "CreatedDate", DbType.DateTime, 30);
+
+            db.ExecuteNonQuery(command, transaction);
+
+            student.CreatedDate = Convert.ToDateTime(db.GetParameterValue(command, "CreatedDate").ToString());
+            student.UpdatedDate = student.CreatedDate;
+
+            return true;
+        }
+
+        public bool Update(Student student, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_StudentUpdate");
+
+            db.AddInParameter(command, "StudentId", DbType.Guid, student.StudentId);
+            db.AddInParameter(command, "UserId", DbType.Guid, student.UserId);
+            db.AddInParameter(command, "SchoolId", DbType.Guid, student.SchoolId);
+            db.AddInParameter(command, "IsDeleted", DbType.String, student.IsDeleted);
+            db.AddInParameter(command, "CreatedBy", DbType.Guid, student.CreatedBy);
+            db.AddInParameter(command, "CreatedDate", DbType.DateTime, student.CreatedDate);
+            db.AddInParameter(command, "UpdatedBy", DbType.Guid, student.UpdatedBy);
+            db.AddInParameter(command, "UpdatedDate", DbType.DateTime, student.UpdatedDate);
+
+            db.AddOutParameter(command, "CreatedDate", DbType.DateTime, 30);
+
+            db.ExecuteNonQuery(command, transaction);
+
+            student.CreatedDate = Convert.ToDateTime(db.GetParameterValue(command, "CreatedDate").ToString());
+            student.UpdatedDate = student.CreatedDate;
+
+            return true;
+        }
+
+        public bool Delete(Student student, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_StudentDelete");
+
+            db.AddInParameter(command, "StudentId", DbType.Guid, student.StudentId);
+
+            db.ExecuteNonQuery(command, transaction);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if user record with UserId exist in User table of the database.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+
+        public bool IsUserExist(Student student)
+        {
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbCommand command = db.GetStoredProcCommand("usp_Student_IsStudentExist");
+
+            db.AddInParameter(command, "StudentId", DbType.Guid, student.StudentId);
+            db.AddOutParameter(command, "IsExist", DbType.Boolean, 1);
+
+            db.ExecuteNonQuery(command);
+
+            return Convert.ToBoolean(db.GetParameterValue(command, "IsExist").ToString());
+        }
+
     }
 }
