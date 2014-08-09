0a6d288ef1bfb3d62dadf64d930926a2738b1174
look2cool <look2cool@gmail.com>
2014-08-10 01:24:40 +0530

    add house DAO and properties

--- RHP.LandlordManagement/House.cs
+++ RHP.LandlordManagement/House.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using RHP.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
 
 namespace RHP.LandlordManagement
 {
@@ -23,5 +25,74 @@ namespace RHP.LandlordManagement
         public string PropertyImagePath { get; set; }
         public decimal? RatingValue { get; set; }
         public decimal? Price { get; set; }
+
+
+        public bool Save()
+        {
+            bool result = false;
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                HouseDAO houseDAO = new HouseDAO();
+                if (houseDAO.IsHouseExist(this))
+                {
+                    result = houseDAO.Update(this, db, transaction);
+                }
+                else
+                {
+                    result = houseDAO.Insert(this, db, transaction);
+                }
+
+                transaction.Commit();
+            }
+            catch (System.Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Delete user from the database.
+        /// This only marks the user as IsDeleted
+        /// </summary>
+        /// <returns></returns>
+        public bool Delete()
+        {
+            bool result = false;
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                result = (new HouseDAO().Delete(this, db, transaction));
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
     }
 }
