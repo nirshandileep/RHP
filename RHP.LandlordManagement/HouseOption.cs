8ae02be9167213a1ac63858e9a385fa85eae539a
nirshandileep <cooldilcool@gmail.com>
2014-08-20 22:21:05 +0530

    House option class missing

---
+++ RHP.LandlordManagement/HouseOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RHP.LandlordManagement
+{
+    public class HouseOption : Common.Base
+    {
+        public Guid HouseOptionId { get; set; }
+        public Guid HouseId { get; set; }
+        public int OptionId { get; set; }
+        public string OptionValue { get; set; }
+    }
+}
