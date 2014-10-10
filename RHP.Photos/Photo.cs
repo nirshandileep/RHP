a7a189ef65847ad7168d21809de15d955cf41d13
look2cool <look2cool@gmail.com>
2014-10-10 17:47:41 +0530

    photo.cs and photoDAO.cs

---
+++ RHP.Photos/Photo.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RHP.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+
+namespace RHP.Photos
+{
+    public class Photo : Base
+    {
+       public Guid? PhotoId { get; set; }
+	   public string FileName  { get; set; }
+	   public string FilePath  { get; set; }
+	   public string Description  { get; set; }
+       public string ContextSubTypeId  { get; set; }
+       public string PhotoCategoryId { get; set; }
+
+        private int _contextTypeId;
+        public int ContextTypeId
+        {
+            get 
+            {
+                return _contextTypeId;
+            }
+            set
+            {
+                ContextType = (Enums.ContextType)value;
+                _contextTypeId = value;
+            }
+        }
+        public Enums.ContextType ContextType { get; set; }
+        public Guid ContextId { get; set; }
+
+        /// <summary>
+        /// Only to be used for Generic methods. Avoid using this constructor for other purposes.
+        /// </summary>
+        public Photo()
+        {
+ 
+        }
+
+        public Photo(Enums.ContextType contextType)
+        {
+            ContextTypeId = (int)contextType;
+            ContextType = contextType;
+        }
+
+        public bool Insert(Photo photo)
+        {
+            bool result = false;
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                result = new PhotoDAO().Insert(photo, db, transaction);
+                transaction.Commit();
+            }
+            catch (System.Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+
+        }
+
+        public bool Update(Photo photo)
+        {
+            bool result;
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                result = new PhotoDAO().Update(photo, db, transaction);
+                transaction.Commit();
+            }
+            catch (System.Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+
+        public bool Delete(Photo photo)
+        {
+            bool result = false;
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbConnection connection = db.CreateConnection();
+            connection.Open();
+            DbTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                result= new PhotoDAO().Delete(photo, db, transaction);
+
+                transaction.Commit();
+            }
+            catch (System.Exception ex)
+            {
+                transaction.Rollback();
+                result = false;
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return result;
+        }
+
+        public Photo(Enums.ContextType contextType, Guid contextId)
+        {
+            ContextTypeId = (int)contextType;
+            ContextType = contextType;
+            ContextId = contextId;
+        }
+
+        public List<Photo> SelectListByStudentId(Guid studentId)
+        {
+            return PhotoDAO.GetAllByFieldValue("ContextId", studentId, Enums.ContextType.Student);
+        }
+
+        public List<Photo> SelectListByLandLordId(Guid landlordId)
+        {
+            return PhotoDAO.GetAllByFieldValue("ContextId", landlordId, Enums.ContextType.Landlord);
+        }
+
+        public DataSet SelectDataSetByStudentId(Guid studentId)
+        {
+
+            return new PhotoDAO().SelectByContext((int)Enums.ContextType.Student, studentId);
+        }
+
+        public DataSet SelectDataSetByLandlordId(Guid landlordId)
+        {
+            return new PhotoDAO().SelectByContext((int)Enums.ContextType.Landlord, landlordId);
+        }
+    }
+}
---
+++ RHP.Photos/PhotoDAO.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using RHP.Common;
+
+namespace RHP.Photos
+{
+    public class PhotoDAO
+    {
+        public DataSet SelectByContext(int contextTypeId, Guid contextId)
+        {
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbCommand command = db.GetStoredProcCommand("usp_PhotoSelectAllByContextId");
+            db.AddInParameter(command, "ContextTypeId", DbType.Int32, contextTypeId);
+            db.AddInParameter(command, "ContextId", DbType.Guid, contextId);
+
+            return db.ExecuteDataSet(command);
+        }
+
+        public bool Insert(Photo photo, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_PhotoInsert");
+            photo.PhotoId = Guid.NewGuid();
+
+            db.AddInParameter(command, "PhotoId", DbType.Guid, photo.PhotoId);
+            db.AddInParameter(command, "FileName", DbType.String, photo.FileName);
+            db.AddInParameter(command, "FilePath", DbType.String, photo.FilePath);
+            db.AddInParameter(command, "ContextId", DbType.Guid, photo.ContextId);
+            db.AddInParameter(command, "Description", DbType.Guid, photo.Description);
+            db.AddInParameter(command, "ContextTypeId", DbType.Int32, (int)photo.ContextType);
+            db.AddInParameter(command, "ContextSubTypeId", DbType.Int32, photo.ContextSubTypeId);
+            db.AddInParameter(command, "PhotoCategoryId", DbType.Int32, photo.PhotoCategoryId);
+            db.AddInParameter(command, "CreatedBy", DbType.Guid, photo.CreatedBy);
+            db.AddOutParameter(command, "CreatedDate", DbType.DateTime, 30);
+
+            if (transaction == null)
+            {
+                db.ExecuteNonQuery(command);
+            }
+            else
+            {
+                db.ExecuteNonQuery(command, transaction);
+            }
+
+            photo.CreatedDate = Convert.ToDateTime(db.GetParameterValue(command, "CreatedDate").ToString());
+            photo.UpdatedDate = photo.CreatedDate;
+
+            return true;
+        }
+
+        public bool Update(Photo photo, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_PhotoUpdate");
+
+            db.AddInParameter(command, "PhotoId", DbType.Guid, photo.PhotoId);
+            db.AddInParameter(command, "FileName", DbType.String, photo.FileName);
+            db.AddInParameter(command, "FilePath", DbType.String, photo.FilePath);
+            db.AddInParameter(command, "ContextId", DbType.Guid, photo.ContextId);
+            db.AddInParameter(command, "Description", DbType.Guid, photo.Description);
+            db.AddInParameter(command, "ContextTypeId", DbType.Int32, (int)photo.ContextType);
+            db.AddInParameter(command, "ContextSubTypeId", DbType.Int32, photo.ContextSubTypeId);
+            db.AddInParameter(command, "PhotoCategoryId", DbType.Int32, photo.PhotoCategoryId);
+
+            db.AddInParameter(command, "UpdatedBy", DbType.Guid, photo.UpdatedBy);
+            db.AddOutParameter(command, "UpdatedDate", DbType.DateTime, 30);
+
+            if (transaction == null)
+            {
+                db.ExecuteNonQuery(command);
+            }
+            else
+            {
+                db.ExecuteNonQuery(command, transaction);
+            }
+
+            photo.UpdatedDate = Convert.ToDateTime(db.GetParameterValue(command, "UpdatedDate").ToString());
+
+            return true;
+        }
+
+        public bool Delete(Photo photo, Database db, DbTransaction transaction)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_PhotoDelete");
+
+            db.AddInParameter(command, "PhotoId", DbType.Guid, photo.PhotoId);
+
+            if (transaction == null)
+            {
+                db.ExecuteNonQuery(command);
+            }
+            else
+            {
+                db.ExecuteNonQuery(command, transaction);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns a collection of T. T must be an Entity class
+        /// </summary>
+        public static List<Photo> GetAllByFieldValue(string fieldName, Guid fieldValue, Enums.ContextType contextType)
+        {
+            List<Photo> returnEntityCollection = new List<Photo>();
+
+            Database db = DatabaseFactory.CreateDatabase(Constants.CONNECTIONSTRING);
+            DbCommand dbCommand = db.GetStoredProcCommand("usp_CommentSelectAllBy" + fieldName);
+
+            db.AddInParameter(dbCommand, fieldName, DbType.Guid, fieldValue);
+            db.AddInParameter(dbCommand, "ContextTypeId", DbType.Int32, (int)contextType);
+
+            using (IDataReader dataReader = db.ExecuteReader(dbCommand))
+            {
+                while (dataReader.Read())
+                {
+                    Photo entity = new Photo();
+
+                    Utility.Generic.AssignDataReaderToEntity(dataReader, entity);
+                    returnEntityCollection.Add(entity);
+                }
+            }
+
+            return returnEntityCollection;
+        }
+    }
+}
