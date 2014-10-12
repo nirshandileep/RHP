850a4a54bf9892512cb7c28fb5419b25da19ceff
nirshandileep <cooldilcool@gmail.com>
2014-10-12 18:43:43 +0530

    Multimedia project added

---
+++ RHP.Multimedia/Photo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RHP.Common;
+
+namespace RHP.Multimedia
+{
+    public class Photo : Base
+    {
+        public Guid PhotoId { get; set; }
+        public string FileName { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string FilePath { get; set; }
+        public int ContextTypeId { get; set; }
+        public Enums.ContextType ContextType { get; set; }
+
+        public int ContextSubTypeId { get; set; }
+
+
+        public int ContextId { get; set; }
+        public int PhotoCategoryId { get; set; }
+        public Enums.PhotoCategory PhotoCategory { get; set; }
+
+        public static Photo Select(Guid photoId)
+        {
+            Photo photo = Utility.Generic.GetByGUID<Photo>(photoId);
+            return photo;
+        }
+
+        public static List<Photo> Select(Guid contextId, int contextTypeId, int contextSubType = 0)
+        {
+            List<Photo> allPhotos = new List<Photo>();
+            return allPhotos;
+        }
+
+        public static List<Photo> SelectAllByContextId(Guid contextId)
+        {
+            List<Photo> allPhotos = new List<Photo>();
+            return allPhotos;
+        }
+    }
+}
---
+++ RHP.Multimedia/PhotoDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+
+namespace RHP.Multimedia
+{
+    public class PhotoDAO
+    {
+
+
+        public bool Insert(Photo photo, Database db, DbTransaction transaction = null)
+        {
+            DbCommand command = db.GetStoredProcCommand("usp_PhotoInsert");
+            photo.PhotoId = Guid.NewGuid();
+
+            db.AddInParameter(command, "PhotoId", DbType.Guid, photo.PhotoId);
+            db.AddInParameter(command, "FileName", DbType.String, photo.FileName);
+            db.AddInParameter(command, "FilePath", DbType.String, photo.FilePath);
+            db.AddInParameter(command, "ContextId", DbType.Guid, photo.ContextId);
+            db.AddInParameter(command, "Description", DbType.String, photo.Description);
+            db.AddInParameter(command, "ContextTypeId", DbType.Int32, (int)photo.ContextType);
+            db.AddInParameter(command, "ContextSubTypeId", DbType.Int32, photo.ContextSubTypeId);
+            db.AddInParameter(command, "PhotoCategoryId", DbType.Int32, (int)photo.PhotoCategory);          
+
+            db.AddInParameter(command, "IsDeleted", DbType.String, photo.IsDeleted);
+            db.AddInParameter(command, "CreatedBy", DbType.Guid, photo.CreatedBy);
+            db.AddOutParameter(command, "CreatedDate", DbType.DateTime, 30);
+
+            if (transaction == null)
+            {
+                db.ExecuteNonQuery(command);
+            }
+            else
+            {
+                db.ExecuteNonQuery(command, transaction);
+            }
+
+            photo.CreatedDate = Convert.ToDateTime(db.GetParameterValue(command, "CreatedDate").ToString());
+            photo.UpdatedDate = photo.CreatedDate;
+
+            return true;
+        }
+
+
+    }
+}
