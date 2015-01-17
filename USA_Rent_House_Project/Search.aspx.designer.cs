a881edccc2e8639d82b60fcc24ebfc736567fece
sampath medawatta <look2cool@gmail.com>
2015-01-17 12:36:45 +0530

    null error handle  comments and student

--- USA_Rent_House_Project/Search.aspx.designer.cs
+++ USA_Rent_House_Project/Search.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace USA_Rent_House_Project {
     public partial class Search {
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
         /// HiddenFieldCurrentStep control.
         /// </summary>
         /// <remarks>
---
+++ USA_Rent_House_Project/WebServices/getAutoCompleteData.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="getAutoCompleteData.asmx.cs" Class="USA_Rent_House_Project.WebServices.getAutoCompleteData" %>
