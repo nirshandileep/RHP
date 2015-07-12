79fecd3789f18ebbe38211913746cddb23019aeb
cnranasinghe <cnranasinghe@yahoo.com>
2015-07-12 10:25:04 +0530

    UI changes

--- USA_Rent_House_Project/Modules/Home.ascx.designer.cs
+++ USA_Rent_House_Project/Modules/Home.ascx.designer.cs
@@ -22,15 +22,6 @@ namespace USA_Rent_House_Project.Modules {
         protected global::System.Web.UI.WebControls.HyperLink StudentLogin;
         
         /// <summary>
-        /// HouseLogin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HouseLogin;
-        
-        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
@@ -38,5 +29,14 @@ namespace USA_Rent_House_Project.Modules {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// HouseLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HouseLogin;
     }
 }
